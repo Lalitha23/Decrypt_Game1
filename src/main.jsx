faf01c08139b518e3939c2f425fd3791eb66bddb
Lalitha Pammi <lalitha2@uw.edu>
2026-03-14 13:23:19 -0700

    Create src/main.jsx file to import and render DecryptGame component

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DecryptGame from './components/DecryptGame';
+
+ReactDOM.render(<DecryptGame />, document.getElementById('root'));
